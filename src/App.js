89a1aca7a5df8e47320f3e4db59c7b6fa5769c17
Madushan Nipuna Shantharathna <madushannipuna1997@gmail.com>
2021-07-09 22:52:54 +0530

    Update app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import './App.css';
 import {BrowserRouter as  Router,Switch, Route} from 'react-router-dom'
-import Test_Homepage from './Pages/Test_Homepage';
 import DropDown from './Pages/DropDownlist';
 import Chart from './Pages/chart';
-import ChartsNavbBar from './Pages/chartNavBar';
 import Online from './Pages/Onlinestatus';
 
 function App() {
   return (
     <>
     <Router>
-    <Route path='/' component={DropDown}/>
+
+        <DropDown/>
+
         <Online />
 
-      <Switch>
+            <Switch>
 
-          <Route path='/chart' component={Chart}/>
-          <Route path='/nav' component={ChartsNavbBar}/>
+              <Route path='/' component={Chart}/>
+             
           
-
-
-      </Switch>
+            </Switch>
 
     </Router>
     </>
