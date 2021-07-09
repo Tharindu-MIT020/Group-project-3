a0662fe5877705817e71cee378bef2b37d239271
Tharindu-MIT020 <59837933+Tharindu-MIT020@users.noreply.github.com>
2021-07-09 22:27:56 +0530

    online statuse

--- src/App.js
+++ src/App.js
@@ -4,16 +4,17 @@ import Test_Homepage from './Pages/Test_Homepage';
 import DropDown from './Pages/DropDownlist';
 import Chart from './Pages/chart';
 import ChartsNavbBar from './Pages/chartNavBar';
+import Online from './Pages/Onlinestatus';
 
 function App() {
   return (
     <>
     <Router>
     <Route path='/' component={DropDown}/>
+        <Online />
+
       <Switch>
 
-       
-         
           <Route path='/chart' component={Chart}/>
           <Route path='/nav' component={ChartsNavbBar}/>
           
---
+++ src/Pages/Onlinestatus.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Onlinestatus.css'
+
+function Onlinestatus() {
+    return (
+        <div className = "card">
+            <label>Today status</label>
+            <p>30</p>
+        </div>
+    )
+}
+
+export default Onlinestatus
